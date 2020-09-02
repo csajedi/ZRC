6bb3d9b591c911f3c9199069791c722cdb18a854
Cameron Sajedi <cameronsajedi@gmail.com>
2020-09-02 15:44:03 -0400

    fix for zrc1-config_minter

--- example/zrc1/configure_minter.js
+++ example/zrc1/configure_minter.js
@@ -25,7 +25,7 @@ async function main() {
     try {
         const contract = zilliqa.contracts.at(nftAddr);
         const callTx = await contract.callWithoutConfirm(
-            'configureMinter',
+            'ConfigureMinter',
             [
                 {
                     vname: 'minter',
